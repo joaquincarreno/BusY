7e4513b46fa6c6bc1a24112b8245693ef09d00c2
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-03-13 11:45:58 +0100

    changed bus model, implemented orientation and smooth movement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DeckGlMap from "./modules/DeckGlMap";
 import ExampleMeshMap from "./modules/ExampleMeshMap";
 import "./App.css";
 
+// time managment constants
+const step = 0.001;
+const intervalMS = 8;
+const loopLength = 1;
+
+class MovingBus {
+  constructor(info) {
+    // console.log(info);
+    this.coordinates = info.steps;
+    this.timestamps = info.timestamps;
+    this.currentStep = 0;
+    this.position = [0, 0];
+    this.orientation = 0;
+  }
+  updateStep(time) {
+    var i = 0;
+    while (time > this.timestamps[i + 1]) {
+      i = i + 1;
+      if (i >= this.timestamps.lenght) {
+        return new Error("time out of range");
+      }
+    }
+    this.currentStep = i;
+  }
+  getRelativeTime(start, end, current) {
+    end = end - start;
+    current = current - start;
+    return current / end;
+  }
+  getCurrentCoordinates(step, relativeTime) {
+    const start = this.coordinates[step];
+    const end = this.coordinates[step + 1];
+    const dx = end[0] - start[0];
+    const dy = end[1] - start[1];
+    this.orientation = Math.atan(dx / dy);
+    return [start[0] + dx * relativeTime, start[1] + dy * relativeTime];
+  }
+  getPosition(time) {
+    this.updateStep(time);
+    const step = this.currentStep;
+    const relTime = this.getRelativeTime(
+      this.timestamps[step],
+      this.timestamps[step + 1],
+      time
+    );
+    return this.getCurrentCoordinates(step, relTime);
+  }
+  getOrientation() {
+    console.log(this.orientation);
+    return (360 * this.orientation) / (2 * Math.PI);
+  }
+}
+
 function App() {
-  const data = [
+  const staticBusData = [
     {
       coordinates: [-70.65387, -33.443018],
       color: [255, 1, 1],
@@ -22,42 +75,59 @@ function App() {
     },
   ];
 
-  const exampleViewState = {
-    longitude: -122.4,
-    latitude: 37.74,
-    zoom: 11,
-    maxZoom: 20,
-    pitch: 30,
-    bearing: 0,
-  };
+  const movingBusData = [
+    {
+      steps: [
+        [-70.64387, -33.453018],
+        [-70.65387, -33.443018],
+        [-70.66387, -33.443018],
+        [-70.65387, -33.433018],
+      ],
+      timestamps: [0, 0.4, 0.9, 1],
+      id: 0,
+    },
+    // { start: [0, 0], end: [0, 0] },
+  ];
+  const movingBus = new MovingBus(movingBusData[0]);
   const viewState = {
     latitude: -33.443018,
     longitude: -70.65387,
-    zoom: 11,
+    zoom: 14,
     minZoom: 2,
     maxZoom: 15,
   };
   const [time, setTime] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((t) => (t + step) % loopLength);
+    }, intervalMS);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const exampleData =
     "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart-stations.json";
 
   const bunnyMesh = "./src/assets/bunny.obj";
-  const busMesh = "./src/assets/bus_mesh/BUS.obj";
+  const busMesh = "./src/assets/bus/JETSET.obj";
   const exampleMesh =
     "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/humanoid_quad.obj";
 
   return (
     <>
-      <div style={{ width: "100vw", height: "90vh", position: "relative" }}>
+      {/*style={{ width: "100vw", height: "90vh", position: "relative" }}*/}
+      <div>
         <DeckGlMap
-          busData={data}
-          mesh={exampleMesh}
+          staticBusData={staticBusData}
+          movingBuses={[movingBus]}
+          movingBusData={movingBusData}
+          mesh={busMesh}
           viewstate={viewState}
           time={time}
         />
       </div>
-      <div style={{ width: "100%", marginTop: "1.5rem" }}>
+      {/* <div style={{ width: "100%", marginTop: "1.5rem" }}>
         <input
           style={{ width: "100%" }}
           type="range"
@@ -69,7 +139,7 @@ function App() {
             setTime(Number(e.target.value));
           }}
         />
-      </div>
+      </div> */}
     </>
   );
 }
--- frontend/src/modules/DeckGlMap.jsx
+++ frontend/src/modules/DeckGlMap.jsx
@@ -3,40 +3,6 @@ import { DeckGL } from "@deck.gl/react";
 import { GeoJsonLayer } from "@deck.gl/layers";
 import { SimpleMeshLayer } from "@deck.gl/mesh-layers";
 import { OBJLoader } from "@loaders.gl/obj";
-// import { Interpolator } from "../utils/Interpolator";
-
-function spaceInterpolator(start, end, time) {
-  return [
-    start[0] + (end[0] - start[0]) * time,
-    start[1] + (end[1] - start[1]) * time,
-  ];
-}
-
-function getRelativeTime(start, end, current) {
-  end = end - start;
-  current = current - start;
-  return current / end;
-}
-
-// steps = [coord1, coord2, coord3]
-// timestamps = [0, 0.4, 1]
-// time = 0.5
-function Interpolator(steps, timestamps, time) {
-  // console.log(steps);
-  // console.log(timestamps);
-  var i = 0;
-  while (time > timestamps[i + 1]) {
-    i = i + 1;
-    if (i >= timestamps.lenght) {
-      return new Error("time out of range");
-    }
-  }
-  console.log(i);
-  const relTime = getRelativeTime(timestamps[i], timestamps[i + 1], time);
-  return spaceInterpolator(steps[i], steps[i + 1], relTime);
-}
-
-// export default Interpolator;
 
 // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 const COUNTRIES =
@@ -58,11 +24,13 @@ const INITIAL_VIEW_STATE = {
 
 function DeckGlMap({
   viewstate = INITIAL_VIEW_STATE,
-  busData = [],
+  staticBusData = [],
+  movingBusData = [],
+  movingBuses = [],
   mesh = null,
   time = 0,
 }) {
-  // console.log(busData);
+  // console.log(staticBusData);
   const countriesLayer = new GeoJsonLayer({
     id: "countries",
     data: COUNTRIES,
@@ -76,42 +44,31 @@ function DeckGlMap({
   });
 
   const busesLayer = new SimpleMeshLayer({
-    id: "buses",
-    data: busData,
+    id: "static-buses",
+    data: staticBusData,
     mesh: mesh,
     loaders: [OBJLoader],
     getPosition: (d) => d.coordinates,
     getColor: (d) => d.color,
-    // getScale: (d) => [10, 10, 10],
     sizeScale: 50,
     visible: true,
     opacity: 0.2,
   });
 
-  const intData = [
-    {
-      steps: [
-        [-70.65387, -33.443018],
-        [-70.66387, -33.443018],
-        [-70.65387, -33.433018],
-      ],
-      timestamps: [0, 0.4, 1],
-    },
-    // { start: [0, 0], end: [0, 0] },
-  ];
+  // const bus = buses[0];
   const movingBus = new SimpleMeshLayer({
-    id: "movingBus",
-    // data: busData,
-    data: intData,
+    id: "moving-buses",
+    data: movingBusData,
     mesh: mesh,
     loaders: [OBJLoader],
 
-    // getPosition: (d) => [d.start[0], d.start[1] + 0.1 * time],
-    getPosition: (d) => {
-      // console.log(d);
-      return Interpolator(d.steps, d.timestamps, time);
+    getPosition: (d) => movingBuses[d.id].getPosition(time),
+
+    getColor: (d) => [255 * time * time, (1 - time) * 255, 255 * time * time],
+    getOrientation: (d) => {
+      // console.log();
+      return [0, 180 - movingBuses[d.id].getOrientation(), 90];
     },
-    getColor: (d) => [255, (1 - time) * 255, 255 * time],
     sizeScale: 50,
     visible: true,
   });
