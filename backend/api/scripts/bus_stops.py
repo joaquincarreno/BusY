7acd43d2eecb960be7a82b424d165e7fcd193084
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-05-13 15:32:13 -0400

    feat: bus stops models now fills on start up

--- backend/api/scripts/bus_stops.py
+++ backend/api/scripts/bus_stops.py
@@ -9,24 +9,31 @@ import os
 from api.scripts.constants import RAW_DATA
 
 
-df = pd.read_excel(RAW_DATA / 'paraderos_2018-1.xlsx')
 
-
-geom =  df.apply(lambda row: Point(row['x'], row['y']), axis=1)
-geopd = gpd.GeoDataFrame(data=df.drop(columns=[
-    'x', 
-    'y', 
-    'Orden\nCirc.', 
-    'Sentido Servicio', 
-    'Varian-te', 
-    'UN', 
-    'Código TS',
-    'Código Usuario',
-    'Comuna',
-    'Eje',
-    'Desde ( Cruce 1)',
-    'Hacia ( Cruce 2)',
-    'Operación con Zona Paga',
-    'Paradas con Excepciones'
-    ]), geometry=geom, crs='epsg:5361')
-uniques = 
+def setupBusStops(model):
+    if(len(model.objects.all()) != 0):
+        return
+    df = pd.read_excel(RAW_DATA / 'paraderos_2018-1.xlsx')
+    uniques = df.drop(columns=[
+        'Orden\nCirc.', 
+        'Sentido Servicio', 
+        'Varian-te', 
+        'UN', 
+        'Código TS',
+        'Código Usuario',
+        'Comuna',
+        'Eje',
+        'Desde ( Cruce 1)',
+        'Hacia ( Cruce 2)',
+        'Operación con Zona Paga',
+        'Paradas con Excepciones']).drop_duplicates(subset='Código paradero TS')
+    for i in uniques.index:
+        row = uniques.iloc[i, :]
+        object = model(
+            TSCode=row['Código paradero TS'],
+            userCode=row['Código Usuario'],
+            name=row['Nombre Paradero'],
+            positionX=row['x'],
+            positionY=row['y']
+        )
+        object.save()
