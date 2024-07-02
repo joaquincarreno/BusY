eac6f932e97061ad85dc487110835b03e0eb2063
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-02 14:38:10 -0400

    feat: Buses ahora usa movingBusAbsolute

--- frontend/src/classes/Buses.js
+++ frontend/src/classes/Buses.js
@@ -1,12 +1,31 @@
 import MovingBus from "./MovingBus";
+import MovingBusAbsolute from "./MovingBusAbsolute";
 class Buses {
   constructor(busList) {
+    // console.log('[busList]', busList)
+    this.earliestTimeStamp = new Date('3000/01/01'); // máxima fecha posible
+    this.latestTimeStamp = new Date('1000/01/01'); // míxima fecha posble
+    
+
     this.dict = {};
     // console.log(busList);
     busList.forEach((b) => {
-      this.dict[b.patente] = new MovingBus(b);
+      this.dict[b.patente] = new MovingBusAbsolute(b);
+      // console.log(new Date(this.dict[b.patente].firstTimeStamp))
+      if(this.dict[b.patente].firstTimeStamp < this.earliestTimeStamp){
+        this.earliestTimeStamp = this.dict[b.patente].firstTimeStamp
+      }
+      
+      if(this.dict[b.patente].lastTimeStamp > this.latestTimeStamp){
+        this.latestTimeStamp = this.dict[b.patente].lastTimeStamp
+      }
     });
-    // console.log(this);
+    
+    this.timeRange = this.latestTimeStamp - this.earliestTimeStamp;
+
+    // console.log('[earliestTimeStamp]', new Date(this.earliestTimeStamp));
+    // console.log('[latestTimeStamp]', new Date(this.latestTimeStamp));
+    // console.log('[timeRange]', this.timeRange)
   }
   getBus(patente) {
     if(patente in this.dict){
---
+++ frontend/src/classes/MovingBusAbsolute.js
@@ -0,0 +1,62 @@
+class MovingBusAbsolute {
+  constructor(info) {
+    this.coordinates = info.coords;
+    // console.log(Date.parse(info.timeStamps[0]))
+    this.timeStamps = info.timeStamps.map((x) => (Date.parse(x)));
+
+    // console.log(this.timeStamps)\
+    this.firstTimeStamp = this.timeStamps[0]
+    this.lastTimeStamp = this.timeStamps[info.timeStamps.length - 1]
+    this.currentStep = 0;
+    this.position = [0, 0];
+    this.orientation = 0;
+    // console.log("-  logs  -");u
+  }
+    updateStep(time) {
+    var i = 0;
+    while (time > this.timeStamps[i + 1]) {
+      i = i + 1;
+      if (i >= this.timeStamps.lenght) {
+        return new Error("time out of range");
+      }
+    }
+    if (this.currentStep != i) {
+      this.currentStep = i;
+    }
+  }
+  getRelativeTime(start, end, current) {
+    end = end - start;
+    current = current - start;
+    return current / end;
+  }
+  getCurrentCoordinates(step, relativeTime) {
+    const start = this.coordinates[step];
+    const end = this.coordinates[step + 1];
+    // console.log("[getCurrentCoords] step count = ");
+    // console.log(this.coordinates.length);
+    const dx = end[0] - start[0];
+    const dy = end[1] - start[1];
+    // console.log("getCurrentCoords: dx=", dx, "dy=", dy);
+    if (dx == 0) {
+      this.orientation = dy >= 0 ? Math.PI : -Math.PI;
+    } else {
+      this.orientation = Math.atan(dy / dx);
+    }
+    return [start[1] + dy * relativeTime, start[0] + dx * relativeTime];
+  }
+  getPosition(time) {
+    this.updateStep(time);
+    const step = this.currentStep;
+    const relTime = this.getRelativeTime(
+      this.timeStamps[step],
+      this.timeStamps[step + 1],
+      time
+    );
+    return this.getCurrentCoordinates(step, relTime);
+  }
+  getOrientation() {
+    return (360 * this.orientation) / (2 * Math.PI);
+  }
+}
+
+export default MovingBusAbsolute;
