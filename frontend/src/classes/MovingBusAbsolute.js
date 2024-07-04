13e0439ec6ce86951bfd67d2a25ee9deeae49262
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-03 20:24:46 -0400

    fix: se cambió el esquema de los buses, ahora hay getters y updaters

--- frontend/src/classes/MovingBusAbsolute.js
+++ frontend/src/classes/MovingBusAbsolute.js
@@ -3,20 +3,62 @@ class MovingBusAbsolute {
     this.patente = info.patente
     this.nSteps = info.coords.length 
     this.coordinates = info.coords;
-    // console.log(Date.parse(info.timeStamps[0]))
     this.timeStamps = info.timeStamps.map((x) => (Date.parse(x)));
 
-    // console.log(this.timeStamps)\
+    // console.log(this.timeStamps)
     this.firstTimeStamp = this.timeStamps[0]
     this.lastTimeStamp = this.timeStamps[this.nSteps - 1]
     this.currentStep = 0;
     this.position = [0, 0];
     this.orientation = 0;
+    this.color = [0, 0, 0, 255]
     // console.log("-  logs  -");u
   }
-    updateStep(time) {
+  // updaters
+  updateBus(time, startingTime){
+    const currentTime = time * 1000 + startingTime
+    console.log('updating', this.patente)
+    console.log('time', time, 'starting time', startingTime)
+    this.updateStep(currentTime);
+    this.updatePosition(currentTime);
+    this.updateColor(currentTime);
+  }
+  updateColor(time){
+    if(time > this.lastTimeStamp || time < this.first){
+      this.color = [0, 0, 0, 0]
+    }else{
+      const relTime = this.getRelativeTime(this.firstTimeStamp, this.lastTimeStamp, time)
+      // console.log(relTime)
+      this.color = [255 * relTime, 255 * relTime, 150]
+    }
+  }
+  updatePosition(time) {
+    const step = this.currentStep;
+    const startTime = this.timeStamps[step];
+    const endTime = this.timeStamps[step + 1];
+    const relTime = (time - startTime) / (endTime - startTime)
+
+    const startPosition = this.coordinates[step];
+    const endPosition = this.coordinates[step + 1];
+    
+    const dx = endPosition[0] - startPosition[0];
+    const dy = endPosition[1] - startPosition[1];
+    
+    if (dx == 0) {
+      this.orientation = dy >= 0 ? Math.PI : -Math.PI;
+    } else {
+      this.orientation = Math.atan(dy / dx);
+    }
+    this.position [startPosition[1] + dy * relTime, startPosition[0] + dx * relTime];
+  }
+  updateStep(time) {
+    console.log('currentTime', time)
+    console.log('as date', new Date(time))
+    console.log('currentStep', this.currentStep)
     if(time < this.timeStamps[0]){
       this.currentStep = 0
+      console.log('time earlier than first step for', this.patente)
+      // console.log(new Date(time), '<' , new Date(this.timeStamps[0]))
       return
     }
     var i = 0;
@@ -24,52 +66,27 @@ class MovingBusAbsolute {
       i = i + 1;
       if (i >= this.nSteps) {
         this.currentStep = this.nSteps-1
+        
+        // console.log('time later than last step for', this.patente)
+        // console.log(new Date(time), '>' , new Date(this.timeStamps[this.nSteps-1]))
         return
       }
     }
     this.currentStep = i;
   }
+
+  //getters
   getRelativeTime(start, end, current) {
-    return (current - start) / (end - start);
-  }
-  getCurrentCoordinates(step, relativeTime) {
-    const start = this.coordinates[step];
-    const end = this.coordinates[step + 1];
-    // console.log("[getCurrentCoords] step count = ");
-    // console.log(this.coordinates.length);
-    const dx = end[0] - start[0];
-    const dy = end[1] - start[1];
-    // console.log("getCurrentCoords: dx=", dx, "dy=", dy);
-    if (dx == 0) {
-      this.orientation = dy >= 0 ? Math.PI : -Math.PI;
-    } else {
-      this.orientation = Math.atan(dy / dx);
-    }
-    return [start[1] + dy * relativeTime, start[0] + dx * relativeTime];
-  }
-  getPosition(time) {
-    this.updateStep(time);
-    const step = this.currentStep;
-    const startTime = this.timeStamps[step];
-    const endTime = this.timeStamps[step + 1];
-    const relTime = this.getRelativeTime(
-      startTime,
-      endTime,
-      time
-    );
-    return this.getCurrentCoordinates(step, relTime);
+    return ;
   }
   getOrientation() {
     return (360 * this.orientation) / (2 * Math.PI);
   }
-  getColor(time){
-    if(time > this.lastTimeStamp || time < this.first){
-      return [0, 0, 0, 0]
-    }else{
-      const relTime = this.getRelativeTime(this.firstTimeStamp, this.lastTimeStamp, time)
-      // console.log(relTime)
-      return [255 * relTime, 255 * relTime, 150]
-    }
+  getPosition(){
+    return this.position;
+  }
+  getColor(){
+    return this.color;
   }
 }
 
