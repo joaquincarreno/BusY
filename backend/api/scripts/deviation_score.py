e418f333f1f48c4fecbc3cce8d97cd493c9de306
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-06 18:37:52 -0400

    feat: calculo de deviationScore

---
+++ backend/api/scripts/deviation_score.py
@@ -0,0 +1,16 @@
+import numpy as np
+import geopandas as gpd
+import pandas as pd
+from shapely import Point, LineString, distance
+
+def calculateDeviationScore(gps, stops):
+    stops_line = LineString(stops)
+
+    distances = [distance(Point(x), stops_line) for x in gps]
+    n = len(distances)
+
+    score = 0
+    for d in distances:
+        score += d/n
+
+    return score
