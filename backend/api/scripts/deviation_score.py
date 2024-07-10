c926186d9e1cb0a24aef925f7037e8f37fab1f17
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-10 16:01:18 -0400

    feat: se extrajo la lógica del cálculo de puntaje de desviación

--- backend/api/scripts/deviation_score.py
+++ backend/api/scripts/deviation_score.py
@@ -6,14 +6,22 @@ from shapely import Point, LineString, distance
 CRS_ORIG = 'EPSG:9152'
 CRS_PROJ = 'EPSG:20048'
 
+def pointListToGdf(points):
+    
+    gdf = gpd.GeoDataFrame(geometry=[Point(x[0], x[1]) for x in points], crs=CRS_ORIG)
+    gdf.to_crs(CRS_PROJ, inplace=True)
+
+    return gdf
+
+def distanceList(gdf, line):
+    return [distance(Point(x), line) for x in gdf['geometry']]
+
 def calculateDeviationScore(gps, stops):
-    # print(gps)
-    gps_df = gpd.GeoDataFrame(geometry=[Point(x[0], x[1]) for x in gps], crs=CRS_ORIG)
-    gps_df.to_crs(CRS_PROJ, inplace=True)
-    stops_df = gpd.GeoDataFrame(geometry=[Point(x[0], x[1]) for x in stops], crs=CRS_ORIG)
-    stops_df.to_crs(CRS_PROJ, inplace=True)
+    gps_df = pointListToGdf(gps)
+    stops_df = pointListToGdf(stops)
+
     stops_line = LineString(stops_df['geometry'])
-    distances = [distance(Point(x), stops_line) for x in gps_df['geometry']]
+    distances = distanceList(gps_df, stops_line)
     n = len(distances)
 
     score = 0
@@ -21,3 +29,4 @@ def calculateDeviationScore(gps, stops):
         score += d/n
 
     return score
+
