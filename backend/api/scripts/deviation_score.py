e7d9da874b5b63d45f683f2f02aed6ca5144dc9f
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-10 10:01:54 -0400

    feat: deviation score se calcula en kilometros

--- backend/api/scripts/deviation_score.py
+++ backend/api/scripts/deviation_score.py
@@ -3,9 +3,17 @@ import geopandas as gpd
 import pandas as pd
 from shapely import Point, LineString, distance
 
+CRS_ORIG = 'EPSG:9152'
+CRS_PROJ = 'EPSG:20048'
+
 def calculateDeviationScore(gps, stops):
-    stops_line = LineString(stops)
-    distances = [distance(Point(x), stops_line) for x in gps]
+    # print(gps)
+    gps_df = gpd.GeoDataFrame(geometry=[Point(x[0], x[1]) for x in gps], crs=CRS_ORIG)
+    gps_df.to_crs(CRS_PROJ, inplace=True)
+    stops_df = gpd.GeoDataFrame(geometry=[Point(x[0], x[1]) for x in stops], crs=CRS_ORIG)
+    stops_df.to_crs(CRS_PROJ, inplace=True)
+    stops_line = LineString(stops_df['geometry'])
+    distances = [distance(Point(x), stops_line) for x in gps_df['geometry']]
     n = len(distances)
 
     score = 0
